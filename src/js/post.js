1ef4de960e5b32e6d00be2c12a343a8ada8526fc
邓红军 <dhjddcn@qq.com>
2023-01-04 17:22:20 +0800

    :construction:  开发文章页面

---
+++ src/js/post.js
@@ -0,0 +1,18 @@
+/**
+ * @Author: 小红
+ * @Date: 2023/1/4
+ * @fileName: post
+ * @Description: 文章
+ */
+
+class Post {
+  constructor() {
+  }
+
+
+}
+
+
+!(() => {
+  document.addEventListener("DOMContentLoaded", () => window.PostClass = new Post())
+})();
