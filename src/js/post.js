72c86913aca5b36fcefe6bab031ef1636e99a1ee
邓红军 <denghongjun@persagy.com>
2023-01-08 01:06:36 +0800

    :construction:  开发 post

--- src/js/post.js
+++ src/js/post.js
@@ -7,15 +7,37 @@
 
 class Post {
   constructor() {
-    console.log('post');
+    this.outDate();
     this.permalinkDecode();
     this.fancyBoxImg();
+  }
+
+  // 文章过期
+  outDate() {
+    const dom = GlobalClass.Butterfly.find('.post-outDate');
+
+    if (!dom.length) return;
+
+    const outDate = Number(dom.attr('data-outDate'));
+    
+    const publishTime = dom.attr('data-publishTime')
+
+    let start = new Date(publishTime);
+    
+    let end = new Date();
+    
+    let days = Math.floor((end - start) / (24 * 3600 * 1000));
+    
+    if (days > outDate) {
+      dom.html(`距离上次更新已经过了 ${days} 天，文中部分内容可能已经过时，如有疑问，请在下方留言。`);
+      dom.removeClass('none')
+    }
   }
 
   // dCodeURI
   permalinkDecode() {
     const dom = $('.copyRight .permalink');
-    
+
     if (!dom.length) return;
 
     dom.html(decodeURIComponent(dom.html()));
@@ -33,9 +55,7 @@ class Post {
       $this.attr('height', '');
       $this.wrap(
         $(
-          `<a class="block text-center w-100" data-fancybox="post" href="${$this.attr(
-            "src"
-          )}"></a>`
+          `<span class="block text-center w-100" data-fancybox="post" ></span>`
         )
       );
     });
