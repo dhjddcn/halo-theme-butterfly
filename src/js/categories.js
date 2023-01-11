63dfeafb05263bd5a3c92f02d06debe913beac86
邓红军 <dhjddcn@qq.com>
2023-01-11 18:10:21 +0800

    :construction:  初始分类

---
+++ src/js/categories.js
@@ -0,0 +1,18 @@
+/**
+ * @Author: 小红
+ * @Date: 2023/1/11
+ * @fileName: categories
+ * @Description: 分类
+ */
+
+class Categories {
+  constructor() {
+
+  }
+
+
+}
+
+!(() => {
+  document.addEventListener("DOMContentLoaded", () => window.CategoriesClass = new Categories())
+})();
