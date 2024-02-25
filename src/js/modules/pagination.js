a444940ddcf5a81ad29ca4db464140d4fbae6cb7
小红 <dhjddcn@qq.com>
2024-02-25 23:09:09 +0800

    :art:  pagination.html

--- src/js/modules/pagination.js
+++ src/js/modules/pagination.js
@@ -9,18 +9,42 @@ import $ from 'jquery';
 
 
 export default class Pagination {
-  #dom = $('.pagination')
+  #dom = $('.pagination');
+
+  #pgn = {};
 
   constructor() {
     this.#init()
   }
 
   #init() {
-    const [path, page, size, total, hasNext, hasPrevious, totalPages, nextUrl, prevUrl] = this.#dom.data('pgn').split('-');
-    
-
-    console.log(page);
+    const [path, nextUrl, prevUrl, page, size, totalPages, total, hasPrevious, hasNext] = this.#dom.data('pgn').split('-');
+    this.#pgn = {path, nextUrl, prevUrl, page, size, totalPages, total, hasPrevious, hasNext};
+    this.#createPage();
   }
 
+  #createPage() {
+    if (this.#pgn.page > this.#pgn.totalPages) {
+      return;
+    }
+
+    let page = '';
+    let next = '';
+    let prev = '';
+
+    // 上一页
+    if (this.#pgn.hasPrevious == 'true') {
+      prev = `<a class="page prev" rel="prev" href="${this.#pgn.prevUrl}"><i class="fas fa-chevron-left fa-fw"></i></a>`;
+    }
+    // 下一页
+    if (this.#pgn.hasNext == 'true') {
+      next = `<a class="page next" rel="next" href="${this.#pgn.nextUrl}"><i class="fas fa-chevron-right fa-fw"></i></a>`;
+    }
+
+    for (let i = 1; i <= this.#pgn.totalPages; i++) {
+      page += `<a class="page ${i == this.#pgn.page ? 'current' : ''}"  href="${this.#pgn.path}/page/${i}">${i}</a>`;
+    }
+    this.#dom.html(prev + page + next);
+  }
 }
  
