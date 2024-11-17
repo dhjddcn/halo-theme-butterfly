998e10467a28a022e5b3b218fb5b2e371263d29d
xiaohong <dhjddcn@qq.com>
2024-11-17 15:02:22 +0800

    fix: vite

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  printWidth: 120,
-  tabWidth: 2,
-  useTabs: false,
-  endOfLine: "lf",
+  printWidth: 120, // 每行最大字符数
+  tabWidth: 2, // 缩进的空格数
+  useTabs: false, // 使用空格代替 tab
+  semi: true, // 语句末尾是否需要分号
+  singleQuote: true, // 是否使用单引号
+  trailingComma: "es5", // 多行对象或数组末尾是否添加逗号: "es5" | "none" | "all"
+  bracketSpacing: true, // 对象文字中是否在括号内添加空格
+  arrowParens: "always", // 箭头函数参数始终使用括号
+  endOfLine: "lf", // 换行符风格: "lf" | "crlf" | "cr" | "auto"
 };
