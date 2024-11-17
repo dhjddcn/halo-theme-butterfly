3bee4c8dcbd5e84ac9f15bf91bd406c151a636df
xiaohong <dhjddcn@qq.com>
2024-11-17 15:32:57 +0800

    fix: vite

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import 'virtual:uno.css';
 import './styles/main.css';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
 
 export function count(x: number, y: number) {
   return x + y;
--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'unocss';
+import { defineConfig, presetUno } from 'unocss';
+import presetIcons from '@unocss/preset-icons';
 
 export default defineConfig({
   content: {
     filesystem: ['./templates/**/*.html', './src/main.ts'],
   },
+  presets: [
+    presetUno(),
+    presetIcons({
+      // scale: 1.2, // 图标的缩放比例
+      // extraProperties: {
+      //   'display': 'inline-block',
+      //   'vertical-align': 'middle', // 图标对齐方式
+      // },
+    }),
+  ],
 });
