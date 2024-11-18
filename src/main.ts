019369ff46134f4831cc222c31e7ffe7a373e4ba
xiaohong <dhjddcn@qq.com>
2024-11-18 21:19:21 +0800

    fix: vite

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import 'virtual:uno.css';
-import './styles/main.css';
+import './styles/main.scss';
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
 
 Alpine.start();
-
 export function count(x: number, y: number) {
   return x + y;
 }
