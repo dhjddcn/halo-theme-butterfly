f0def713df4a8fc6b8149456d3aae341c76bdaa3
小红 <dhjddcn@qq.com>
2025-03-12 20:19:19 +0800

    fix: dev

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import 'virtual:uno.css';
 import './style/main.scss';
 import useTheme from './modules/Theme';
 import useHeader from './modules/Header';
+import useAction from './modules/Action';
 
 export const theme = new useTheme();
 export const header = new useHeader();
+export const action = new useAction();
---
+++ src/modules/Action/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @date: 2025/3/12
+ * @author: 小红
+ * @fileName: index
+ * @Description: 一些行为
+ */
+
+export default class Action {
+
+  /**
+   * 回到顶部
+   */
+  public  backTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // 平滑滚动
+    });
+  }
+}
