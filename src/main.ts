8a158cb6f68a20b02347d4207971726ef6cd8f23
小红 <dhjddcn@qq.com>
2025-03-18 00:00:00 +0800

    fix: dev

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import './style/main.scss';
 import useTheme from './modules/Theme';
 import useHeader from './modules/Header';
 import useAction from './modules/Action';
+import useMain from './modules/Main';
 
 export const theme = new useTheme();
-export const header = new useHeader();
 export const action = new useAction();
+export const header = new useHeader();
+export const main = new useMain();
---
+++ src/modules/Main/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @date: 2025/3/17
+ * @author: 小红
+ * @fileName: index
+ * @Description: main
+ */
+
+
+export default class Main {
+
+}
+ 
